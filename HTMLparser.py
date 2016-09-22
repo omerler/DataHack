654dccb4cde803318669960f085f3af620f8ffb3
omerler <omerler@gmail.com>
2016-09-22 20:40:05 +0300

    update two files- HTML and parser

--- HTMLparser.py
+++ HTMLparser.py
@@ -1,7 +1,8 @@
 import re
 
 def HTMLtoCodeAndTextParser(HtmlText):
-    # The function get a string represent HTML lines and an array with a tag ("code" or "text"), when single word code are exclude
+    # The function get a string represent HTML lines and an array with a tag
+    #  ("code" or "text"), when single word code are exclude
     outputArray = []
     HtmlText = HtmlText.replace('</code></pre>', '\n').replace('<pre><code>',
          '\n<pre><code>').replace('<p>', '').replace('</p>',
@@ -13,7 +14,7 @@ def HTMLtoCodeAndTextParser(HtmlText):
             outputArray.append(("text", line))
     cleanCode = []
     for element in outputArray:
-        element = (element[0], re.sub(r'<[\w\\]*>', '', element[1]))
+        element = (element[0], re.sub(r'<[\w/]*>', '', element[1]))
         cleanCode.append(element)
     cleanCode[:] = [item for item in cleanCode if item[1] != '']
     return cleanCode
--- parsingMethods.py
+++ parsingMethods.py
@@ -1,10 +1,12 @@
 import javalang
+import re
 
 
 class Argument(object):
     def __init__(self, name, type):
         self.name = name
         self.type = type
+
 
 class Method(object):
     def __init__(self, name, return_type, arguments, throws):
@@ -15,15 +17,17 @@ class Method(object):
 
 
 def is_legitimate_method(method):
-    return "public static main" not in method.name
+    return "static main" not in method.name
 
 # Parsing methods
+
 
 def parse_type(type):
     if type is None:
         return 'void'
     else:
         return type.name + (len(type.dimensions) * '[]')
+
 
 def parse_parameters(parameters):
     return ', '.join(['%s %s' % (parse_type(param.type), param.name) for param in parameters])
@@ -36,17 +40,42 @@ def parse_throws(throws):
         return 'throws ' + str(throws)
 
 
-
 def parse_method(method_node):
     return Method(method_node.name, (parse_type(
         method_node.return_type)),
                   parse_parameters(method_node.parameters),
                   parse_throws(method_node.throws))
 
-# Tester
+
+def extract_methods(contentArray):
+    '''
+    return function daclerations as methods
+    '''
+    methodsArr = []
+    for data in contentArray:
+        if data[0] == "text":
+            continue
+        else:  # data is from code type.
+            try:
+                if 'class' not in data[1]:
+                    data = (data[0], "public class module {\n" + data[1] +
+                            "\n}")
+                tree = javalang.parse.parse(data[1])
+                for i, (path, node) in enumerate(tree):
+                    if isinstance(node, javalang.tree.MethodDeclaration):
+                        method = parse_method(node)
+                        if is_legitimate_method(method):
+                            methodsArr.append(method)
+            except:
+                continue  # if code is written with mistakes - skip to the
+                # next code line. todo specificity in order to extand.
+    return methodsArr
+
 # with open(r'example2.java', 'r') as f:
 #     code = f.read()
 #     tree = javalang.parse.parse(code)
 #     for i, (path, node) in enumerate(tree):
 #         if isinstance(node, javalang.tree.MethodDeclaration):
 #             method = parse_method(node)
+
+
