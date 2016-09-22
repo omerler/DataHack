d64a9685a11c7a591f0d581ac713dbb8b6b74202
litmanovitziv <litmanovitziv@gmail.com>
2016-09-22 22:40:13 +0300

    Updating Training Set

--- createTrainingSet.py
+++ createTrainingSet.py
@@ -15,21 +15,29 @@ if __name__ == "__main__":
     output_file = os.path.join(sys.argv[1], sys.argv[3])
 
     with open(output_file, 'w') as output_file:
-        methods_per_posts = defaultdict(list)
         csv_writer = csv.writer(output_file)
 
         # TODO do something with duplicates
+        # methods_per_posts = defaultdict(list)
+        total_positive_features = 0
+        total_negative_features = 0
 
         prev_post = None
-        for post in get_posts(input_file):
+        for i, post in enumerate(get_posts(input_file)):
             for answer in post.answers:
                 for method in extract_methods(answer.content):
+                    print "post: %s, answer: %s, method: %s" % (post.question.id, answer.id, method.name)
                     feature_positive_vector = extract_feature_vector(method, post, answer)
                     write_dataset_record(csv_writer, feature_positive_vector, label = 1)
+                    total_positive_features += 1
                     if prev_post is not None:
+                        print "post: %s, answer: %s, method: %s" % (prev_post.question.id, answer.id, method.name)
                         feature_negative_vector = extract_feature_vector(method, prev_post, answer)
                         write_dataset_record(csv_writer, feature_negative_vector, label = 0)
-                    methods_per_posts[post] += [method]
+                        total_negative_features += 1
+                    # methods_per_posts[post] += [method]
             prev_post = post
 
-        all_posts_with_methods = list(methods_per_posts.keys())
+        print "total of positive features: %d" % total_positive_features
+        print "total of negative features: %d" % total_negative_features
+        # all_posts_with_methods = list(methods_per_posts.keys())
