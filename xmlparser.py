f60dcd52be49076ff053209757d1398bf8def7cf
litmanovitziv <litmanovitziv@gmail.com>
2016-09-22 03:20:49 +0300

    adding filters and construct post

--- xmlparser.py
+++ xmlparser.py
@@ -1,30 +1,38 @@
 import os, json
 from lxml import etree
 
-DIR = r'C://downloads/code_project/'
+DIR = r'/media/zivlit/Seagate Expansion Drive/Projects/'
 RAW_XML_PATH = os.path.join(DIR, 'Posts.xml')
 
 MAX_ID = 40000000
 
 total_taken_posts = 0
-required_post_ids = set()
+required_post_ids = {}
+
 
 def parse_tags(raw_tags):
     return list(set(raw_tags[1:-1].split('><')))
 
-def should_take(row):
-    if row.attrib['PostTypeId'] == '1':
-        if 'java' in parse_tags(row.attrib['Tags']):
-            required_post_ids.add(row.attrib['Id'])
-            return True
-        else:
-            return False
-    elif row.attrib['PostTypeId'] == '2':
-        return row.attrib['ParentId'] in required_post_ids
-    else:
-        return False
 
-with open(os.path.join(DIR, 'filtered_posts.json'), 'w') as output:
+def filtering(row):
+    id = row.attrib['Id']
+    if row.attrib['PostTypeId'] == '1': # Query
+        tags = parse_tags(row.attrib['Tags'])
+        if 'java' in tags and 'android' not in tags:
+            required_post_ids[id] = {'query': dict(row.attrib), 'count': int(row.attrib['AnswerCount']), 'answers': []}
+    elif row.attrib['PostTypeId'] == '2':   # Answer
+        parent_id = row.attrib['ParentId']
+        if parent_id in required_post_ids:
+            required_post_ids[parent_id]['answers'].append(dict(row.attrib))
+            required_post_ids[parent_id]['count'] -= 1
+            if required_post_ids[parent_id]['count'] == 0:
+                ans = required_post_ids[parent_id]['query']
+                ans['answers'] = required_post_ids[parent_id]['answers']
+                del required_post_ids[parent_id]
+                return ans
+    return False
+
+with open(os.path.join('./', 'output.json'), 'w') as output:
     
     entries_context = etree.iterparse(RAW_XML_PATH, events = ['end'], tag = 'row')
     
@@ -34,13 +42,14 @@ with open(os.path.join(DIR, 'filtered_posts.json'), 'w') as output:
             print 'Took %d posts' % total_taken_posts
             print 'Current ID: %s (%d%%)' % (row.attrib['Id'], int(100.0 * float(row.attrib['Id']) / MAX_ID))
             
-        if should_take(row):
+        record = filtering(row)
+        if record:
             total_taken_posts += 1
-            json.dump(dict(row.attrib), output)
+            json.dump(record, output)
             output.write('\n')
         
         del event
         row.clear()
         del row
-        
+
     del entries_context
