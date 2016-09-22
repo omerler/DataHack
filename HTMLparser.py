f7d87d74520357680a6b519010472345be673ec0
Omer Lerinman <omerler@gmail.com>
2016-09-22 23:57:31 +0300

    Update HTMLparser.py

--- HTMLparser.py
+++ HTMLparser.py
@@ -1,4 +1,21 @@
 import re
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+        
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
 
 def HTMLtoCodeAndTextParser(HtmlText):
     # The function get a string represent HTML lines and an array with a tag
@@ -14,7 +31,8 @@ def HTMLtoCodeAndTextParser(HtmlText):
             outputArray.append(("text", line))
     cleanCode = []
     for element in outputArray:
-        element = (element[0], re.sub(r'<[\w/]*>', '', element[1]))
+        # element = (element[0], re.sub(r'<[\w\\]*>', '', element[1]))
+        element = (element[0], strip_tags(element[1]))
         cleanCode.append(element)
     cleanCode[:] = [item for item in cleanCode if item[1] != '']
     return cleanCode
