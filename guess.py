3fab5be5b96518af0b192a2c3da4e21860e82151
Nadav Brandes <nadavbrandes@gmail.com>
2016-09-23 07:23:00 +0300

    Fixed    guess.py

--- guess.py
+++ guess.py
@@ -1,61 +1,76 @@
+import os
+import sys
+import json
+import operator
+import javalang
+import pickle
+
+import numpy as np
+
+from stackoverflow import get_posts
 from parsingMethods import *
 from semantics import *
-import json
-from stackoverflow import get_posts
-import operator
+from constants import DIR
 
-import javalang
+sys.argv = [None, 'public static JButton createButton(Graphics graphics){}'] # XXX
+
+with open(os.path.join(DIR, 'model.pkl'), 'r') as f:
+    model = pickle.load(f)
 
 def getMethod(userMethodSignature):
-    try:
-        userMethodSignature = "public class module {\n" + userMethodSignature + "\n}"
-        tree = javalang.parse.parse(userMethodSignature)
-        for path, node in tree:
-            if isinstance(node, javalang.tree.MethodDeclaration):
-                method = parse_method(node)
-                return method
-        return None
-    except:
-        return None
+    userMethodSignature = "public class module {\n" + userMethodSignature + "\n}"
+    tree = javalang.parse.parse(userMethodSignature)
+    for path, node in tree:
+        if isinstance(node, javalang.tree.MethodDeclaration):
+            method = parse_method(node)
+            return method
 
 def getMethodTokens(method):
     method_name_tokens = get_tokens_frequency_of_text(method.name)
     method_arg_name_tokens = get_tokens_frequency([argument.name for argument in method.arguments])
     method_arg_type_tokens = get_tokens_frequency([argument.type for argument in method.arguments])
     method_return_type_token = {method.return_type: 1.0}
-    return method_name_tokens, method_arg_name_tokens, \
-           method_arg_type_tokens, method_return_type_token
+    return set(method_name_tokens) | set(method_arg_name_tokens) | set(method_arg_type_tokens) | set(method_return_type_token)
 
-
-def findHueristic(userMethodSignature):
+def findHueristic(method):
+    
     nearestPost = []
-    userTokens = set(getMethodTokens(userMethodSignature))
-    for post in get_posts(constants.FILTERED_POSTS):
-        if set(tokenize_text(post.title)) & userTokens > 0 and \
-            set(tokenize_text(get_stackoverflow_message_text(post.question))) \
-                        & userTokens  > 1\
-            and set.union([tokenize_text(get_stackoverflow_message_text(
-                    answer)) for answer in post.answers]) & userTokens > 1:
+    userTokens = set(getMethodTokens(method))
+    
+    for i, post in enumerate(get_posts(constants.FILTERED_POSTS)):
+    
+        title_shared_tokens = len(set(tokenize_text(post.title)) & userTokens)
+        question_shared_tokens = len(set(tokenize_text(get_stackoverflow_message_text(post.question))) & userTokens)
+        answers_shared_tokens = len(set.union(*[set(tokenize_text(get_stackoverflow_message_text(answer))) for answer in post.answers]) & userTokens)
+        
+        if title_shared_tokens > 0 and question_shared_tokens > 0 and answers_shared_tokens > 1:
             nearestPost.append(post)
-    print (len(nearestPost)) ###
+        
+        if i % 10000 == 0:
+            print 'Processed %d posts' % i
+        
+        # XXX
+        if i > 10000:
+            break
+    
+    print len(nearestPost) # XXX
     return nearestPost
 
 def secondSort(nearestPost, method):
     postsDict = {}
     for post in nearestPost:
-        num = predict_proba(extract_feature_vector(method, post)) ###
-        postsDict[post] = num
-    return sorted(postsDict.items(), key=operator.itemgetter(1), reverse=True)[:10]
-
+        features = np.array([extract_feature_vector(method, post)])
+        features[np.isnan(features)] = -1
+        prob = model.predict_proba(features)[0][1]
+        postsDict[post.title] = prob
+    return sorted(postsDict.items(), key=operator.itemgetter(1), reverse=True)#[:10]
 
 def manager(userMethodSignature):
     method = getMethod(userMethodSignature)
-    if not method:
-        return "problem with method syntax"
-    nearestPosts = findHueristic(userMethodSignature)
+    nearestPosts = findHueristic(method)
     return secondSort(nearestPosts, method)
-
-
-
-
-
+    
+if __name__ == '__main__':
+    userMethodSignature = ' '.join(sys.argv[1:])
+    best_posts = manager(userMethodSignature)
+    print '\n'.join(['[%f] %s' % (prob, post_title) for post_title, prob in best_posts])
